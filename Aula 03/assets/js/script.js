754d5de93fe6bf4133c9f62bf61eaace7a4a2323
KaueMagnus <kmarquesmagnus@gmail.com>
2024-08-26 21:48:34 -0300

    add: Aula 02, Aula 03 e Aula 04

---
+++ Aula 03/assets/js/script.js
@@ -0,0 +1,8 @@
+function verMaisFotos() {
+  let div = document.querySelector("#moreContent")
+  div.style.display = "block"
+
+  let button = document.querySelector(".button-mais-fotos")
+  button.style.display = "none"
+}
+
---
+++ Aula 04/assets/js/script.js
@@ -0,0 +1,46 @@
+function navigate(page) {
+  document.querySelectorAll('main > section').forEach(section => {
+    section.classList.add('disable');
+  });
+  document.getElementById(page).classList.remove('disable')
+}
+
+function submitForm() {
+  const contato = {
+    nome : document.getElementById('nome').value,
+    email : document.getElementById('email').value,
+    phone : document.getElementById('phone').value
+  };
+  exibirContato(contato);
+}
+
+function exibirContato(contato) {
+  const tabela = `
+    <table>
+      <thead>
+      <tr>
+        <th>Nome</th>
+        <th>Email</th>
+        <th>Telefone</th>
+      </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>${contato.nome}</td>
+          <td>${contato.email}</td>
+          <td>${contato.phone}</td>
+        </tr>
+      </tbody>
+    </table>
+  `
+
+  document.getElementById('tabelaContato').innerHTML = tabela;
+}
+
+const menuToggle = document.getElementById('menu-toggle');
+const menu = document.querySelector('#menu ul');
+  
+menuToggle.addEventListener('click', () => {
+     menu.classList.toggle('disable');
+  });
+
